5ae945b5d8b2e764215155e76e3debb5a1f233be
Windows 10 <Windows 10@DESKTOP-ANGJ5V6>
2022-12-07 17:19:48 -0300

    Update

--- loja/src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
@@ -3,10 +3,10 @@ package br.com.alura.loja.testes;
 import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
+import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProdutos {
 
@@ -17,12 +17,12 @@ public class CadastroDeProdutos {
 		celular.setDescricao("Muito Legal");
 		celular.setPreco(new BigDecimal("2800"));
 
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
 		
-		EntityManager em = factory.createEntityManager();
-		
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao dao = new ProdutoDao(em);
+
 		em.getTransaction().begin();
-		em.persist(celular);
+		dao.cadastrar(celular);
 		em.getTransaction().commit();
 		em.close();
 	}
