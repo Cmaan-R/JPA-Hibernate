d703d08edfb3b48734b0e19e6f34d880eda936c2
Windows 10 <Windows 10@DESKTOP-ANGJ5V6>
2022-12-08 20:51:28 -0300

    Update

--- loja/src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
@@ -1,13 +1,8 @@
 package br.com.alura.loja.testes;
 
-import java.math.BigDecimal;
-
 import javax.persistence.EntityManager;
 
-import br.com.alura.loja.dao.CategoriaDao;
-import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Categoria;
-import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProdutos {
@@ -15,19 +10,17 @@ public class CadastroDeProdutos {
 	public static void main(String[] args) {
 
 		Categoria celulares = new Categoria("CELULARES");
-		Produto celular = new Produto("Samsung S22", "Muito Legal", new BigDecimal("2800"), celulares);
 
 		EntityManager em = JPAUtil.getEntityManager();
-		ProdutoDao produtoDao = new ProdutoDao(em);
-		CategoriaDao categoriaDao = new CategoriaDao(em);
-
 		em.getTransaction().begin();
 
-		categoriaDao.cadastrar(celulares);
-		produtoDao.cadastrar(celular);
+		em.persist(celulares);
+		celulares.setNome("XPTO");
 
 		em.getTransaction().commit();
 		em.close();
+
+		celulares.setNome("Aparelho Celulares");
 	}
 
 }
