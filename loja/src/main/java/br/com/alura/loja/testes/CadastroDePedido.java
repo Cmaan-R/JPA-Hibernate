1101d92b3bc290ba1aaf73b9872e0990190fd1e3
Windows 10 <Windows 10@DESKTOP-ANGJ5V6>
2022-12-10 16:52:00 -0300

    Update

--- loja/src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
@@ -1,6 +1,7 @@
 package br.com.alura.loja.testes;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
@@ -24,26 +25,45 @@ public class CadastroDePedido {
 		ClienteDao clienteDao = new ClienteDao(em);
 
 		Produto produto = produtoDao.buscarPorId(1l);
+		Produto produto2 = produtoDao.buscarPorId(2l);
+		Produto produto3 = produtoDao.buscarPorId(3l);
+
 		Cliente cliente = clienteDao.buscarPorId(1l);
 
 		em.getTransaction().begin();
 
 		Pedido pedido = new Pedido(cliente);
 		pedido.adicionarItem(new ItemPedido(10, pedido, produto));
+		pedido.adicionarItem(new ItemPedido(40, pedido, produto2));
+
+		Pedido pedido2 = new Pedido(cliente);
+		pedido2.adicionarItem(new ItemPedido(2, pedido, produto3));
 
 		PedidoDao pedidoDao = new PedidoDao(em);
 		pedidoDao.cadastrar(pedido);
+		pedidoDao.cadastrar(pedido2);
 
 		em.getTransaction().commit();
 		BigDecimal totalVendido = pedidoDao.valorTotalVendido();
 		System.out.println("O Valor total: " + totalVendido);
 
+		List<Object[]> relatorio = pedidoDao.relatorioDeVendas();
+		for (Object[] obj : relatorio) {
+			System.out.println(obj[0]);
+			System.out.println(obj[1]);
+			System.out.println(obj[2]);
+		}
 		em.close();
 	}
 
 	private static void popularBancoDeDados() {
 		Categoria celulares = new Categoria("CELULARES");
+		Categoria videogames = new Categoria("VIDEOGAMES");
+		Categoria informatica = new Categoria("INFORMATICA");
+
 		Produto celular = new Produto("Samsung s22", "Muito Bom", new BigDecimal(2500), celulares);
+		Produto videogame = new Produto("PS5 ", "Playstation 5", new BigDecimal(5000), videogames);
+		Produto macbook = new Produto("MACBOOK ", "Macbook", new BigDecimal(8900), informatica);
 
 		Cliente cliente = new Cliente("Rodrigo", "123456");
 
@@ -55,7 +75,13 @@ public class CadastroDePedido {
 		em.getTransaction().begin();
 
 		categoriaDao.cadastrar(celulares);
+		categoriaDao.cadastrar(videogames);
+		categoriaDao.cadastrar(informatica);
+
 		produtoDao.cadastrar(celular);
+		produtoDao.cadastrar(videogame);
+		produtoDao.cadastrar(macbook);
+
 		clienteDao.cadastrar(cliente);
 
 		em.getTransaction().commit();
