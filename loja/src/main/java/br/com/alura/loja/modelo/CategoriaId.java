45485118581348a500ab4403131228183fbe5ccd
Windows 10 <Windows 10@192.168.200.171>
2022-12-14 10:54:03 -0300

    Update

--- loja/src/main/java/br/com/alura/loja/modelo/CategoriaId.java
+++ loja/src/main/java/br/com/alura/loja/modelo/CategoriaId.java
@@ -1,9 +1,11 @@
 package br.com.alura.loja.modelo;
 
+import java.io.Serializable;
+
 import javax.persistence.Embeddable;
 
 @Embeddable
-public class CategoriaId {
+public class CategoriaId implements Serializable{
 
 	private String nome;
 	private String tipo;
